281a6d5d253ea7cdd7951d41cb182f5bfd3cc38b
Vin Halbwachs <vhalbwachs@gmail.com>
2014-06-26 16:27:18 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -130,7 +130,7 @@ fi
 # 5. Run grunt
 if [ -e "$DEPLOYMENT_TARGET/Gruntfile.js" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install grunt-cli
+  eval $NPM_CMD install grunt-cli grunt
   exitWithMessageOnError "installing grunt failed"
   # echo got to line 135
   ./node_modules/.bin/grunt --no-color build > grunt_output.txt 2>&1
