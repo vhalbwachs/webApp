1e0caeba4b2be3c787457a5ea1451ca1989dcd2b
Vin Halbwachs <vhalbwachs@gmail.com>
2014-06-25 22:23:06 -0700

    More Debugging

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+  // require('time-grunt')(grunt);
 
   // Define the configuration for all the tasks
   grunt.initConfig({
