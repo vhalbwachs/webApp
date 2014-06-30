2a40cd160787f9eccc8e5e82d7f5338e279f0388
Vin Halbwachs <vhalbwachs@gmail.com>
2014-06-30 10:52:27 -0700

    test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express'),
  */
 
 // Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 var config = require('./lib/config/config');
 var db = mongoose.connect(config.mongo.uri, config.mongo.options);
